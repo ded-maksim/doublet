af1ba1d2e3e6a3902a575d86ffa964e9975ec2a4
ded-maksim <36963568+ded-maksim@users.noreply.github.com>
2018-11-12 09:20:48 +0400

    card rotation

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.memory-card');
+
+    function flipCard() {
+        this.classList.toggle('flip');
+    }
+
+    cards.forEach(card => card.addEventListener('click', flipCard));
+});
